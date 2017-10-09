c896bcad5cb26f5c3779c6b12b82eda30d451c3a
priba <priba@cvc.uab.es>
2017-10-09 21:07:16 +0200

    Batch Hausdorff distance

--- options.py
+++ options.py
@@ -22,7 +22,7 @@ class Options():
         # Model parameters
         parser.add_argument('--nlayers', type=int, default=2, help='Message passing + Update layers.')
         # Optimization options
-        parser.add_argument('--epochs', '-e', type=int, default=1, help='Number of epochs to train.')
+        parser.add_argument('--epochs', '-e', type=int, default=100, help='Number of epochs to train.')
         parser.add_argument('--batch_size', '-b', type=int, default=64, help='Batch size.')
         parser.add_argument('--learning_rate', '-lr', type=float, default=1e-2, help='The Learning Rate.')
         parser.add_argument('--momentum', '-m', type=float, default=0.9, help='Momentum.')
@@ -35,7 +35,7 @@ class Options():
         parser.add_argument('--load', '-l', type=str, default=None, help='Checkpoint path to resume / test.')
         parser.add_argument('--test', '-t', action='store_true', help='Test only flag.')
         # Acceleration
-        parser.add_argument('--ngpu', type=int, default=0, help='0 = CPU, 1 = CUDA, 1 < DataParallel')
+        parser.add_argument('--ngpu', type=int, default=1, help='0 = CPU, 1 = CUDA, 1 < DataParallel')
         parser.add_argument('--prefetch', type=int, default=2, help='Pre-fetching threads.')
         # i/o
         parser.add_argument('--log', type=str, default='./log/', help='Log folder.')
